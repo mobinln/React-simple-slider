eb42068406c15c8bee3d497e5d81b869b18dcb97
mobin <mnlarijanian@gmail.com>
2021-02-28 18:19:56 +0330

    emoji for next and prev

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-import item1 from './assets/1.jpg';
-import item2 from './assets/2.jpg';
-import item3 from './assets/3.jpg';
-import item4 from './assets/4.jpg';
+import item1 from "./assets/1.jpg";
+import item2 from "./assets/2.jpg";
+import item3 from "./assets/3.jpg";
+import item4 from "./assets/4.jpg";
 
-const imgs = [
-  item1,
-  item2,
-  item3,
-  item4
-]
+const imgs = [item1, item2, item3, item4];
 
 function App() {
   const [index, setIndex] = useState(0);
@@ -25,13 +20,15 @@ function App() {
 
   return (
     <div className="App">
-      <div className="Slider" style={{
-        background: `url(${imgs[index < 0 ? index + 4 : index]})`, 
-      }}>
-
-        <div className='btns'>
-          <button onClick={handlePrev}>Prev</button>
-          <button onClick={handleNext}>Next</button>
+      <div
+        className="Slider"
+        style={{
+          background: `url(${imgs[index < 0 ? index + 4 : index]})`,
+        }}
+      >
+        <div className="btns">
+          <button onClick={handlePrev}>👈</button>
+          <button onClick={handleNext}>👉</button>
         </div>
       </div>
     </div>
